6151751b3896df17f1e94b5fa1dada0bca6a32cb
Min Seo <seominwoong1@gmail.com>
2025-03-26 20:55:54 +0000

    Change: Cleanup

--- Server/machine_learning.py
+++ Server/machine_learning.py
@@ -29,7 +29,6 @@ def model_training():
 
     y_train = y_scaler.fit_transform(y_train)
 
-    # We then create the model and train it.
     batch_size = 32
     epochs = 20
 
@@ -40,31 +39,17 @@ def model_training():
     model.add(Dense(64, activation="relu"))
     model.add(Dropout(0.2))
 
-    # Note here, we only use one neuron in the output
-    # layer and use a linear activation function.
     model.add(Dense(1, activation="linear"))
 
-    # We use a Mean Squared Error - mse - loss.
     model.compile(loss="mse", optimizer=SGD())
 
     model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.1)
 
-    # With the model trained we can evaluate it on the test set
     x_test = x_scaler.transform(x_test)
     y_test = y_scaler.transform(y_test)
 
     print("\n|| ============== model evaluation ============== ||")
     model.evaluate(x_test, y_test)
-
-    # If we have a new sample we can obtain the median
-    # house price like:
-
-    # new_sample = [[2025, 3, 17, 128.1199951171875]]
-    # new_sample_normalised = x_scaler.transform(new_sample)
-    # raw_output = model.predict(new_sample_normalised)
-    # output = y_scaler.inverse_transform(raw_output)
-    # print("============== output ==============")
-    # print(output)
 
     return model
 
